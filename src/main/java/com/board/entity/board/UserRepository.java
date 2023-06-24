127eadff40e77203825accce6783e2da448f0d08
SunYub <goggi772@gmail.com>
2023-06-25 01:54:34 +0900

    User Repository and Service

--- src/main/java/com/board/entity/board/UserRepository.java
+++ src/main/java/com/board/entity/board/UserRepository.java
@@ -1,4 +1,21 @@
 package com.board.entity.board;
 
-public class  UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    static final String UPDATE_USER_LAST_LOGIN = "UPDATE User SET LAST_LOGIN_TIME = :lastLoginTime WHERE EMAIL = :email";
+
+    @Transactional //트랜잭션 설정(에러발생시 DB rollback)
+    @Modifying //DB 테이블 변경하는 쿼리 작성시 적용해야 하는 annotation
+    @Query(value = "UPDATE_USER_LAST_LOGIN", nativeQuery = true) //일반 sql쿼리 적용s
+    public int updateUserLastLogin(@Param("email") String email, @Param("lastLoginTime") LocalDateTime lastLoginTime);
+
+    public User findByEmail(String email);
 }
--- src/main/java/com/board/service/UserService.java
+++ src/main/java/com/board/service/UserService.java
@@ -1,22 +1,26 @@
 package com.board.service;
 
 import com.board.entity.board.User;
+import com.board.entity.board.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class UserService implements UserDetailsService {
 
-//    public UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-//        User user = useRepository.findByEmail(email);
-//        if (user = null)
+        User user = userRepository.findByEmail(email);
 
-        return null;
+        if (user == null) throw new UsernameNotFoundException("Not Found User");
+
+        return user;
     }
 }
